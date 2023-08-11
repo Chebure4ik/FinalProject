fb3dfce73c06b36ed95dc7bb792965bce5a0f603
Chebure4ik <darikmlk@gmail.com>
2023-08-11 19:44:49 +0600

    commit110823.02

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -9,39 +9,32 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button twoPlayerButton, threePlayerButton, fourPlayerButton;
+    int timer = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        twoPlayerButton = findViewById(R.id.twoPlayerButton);
-        threePlayerButton = findViewById(R.id.threePlayerButton);
-        fourPlayerButton = findViewById(R.id.fourPlayerButton);
 
-        twoPlayerButton.setOnClickListener(listener);
-        threePlayerButton.setOnClickListener(listener);
-        fourPlayerButton.setOnClickListener(listener);
-    }
-    private View.OnClickListener listener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
+        Thread thread = new Thread(){
 
-            switch (view.getId()){
-                case R.id.twoPlayerButton:
-                    Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
-                    startActivity(intent);
-                    break;
-                case R.id.threePlayerButton:
-                    intent = new Intent(getApplicationContext(), ThirdActivity.class);
-                    startActivity(intent);
-                    break;
-                case R.id.fourPlayerButton:
-                    intent = new Intent(getApplicationContext(), FourthActivity.class);
-                    startActivity(intent);
-                    break;
+            public void run(){
+                try {
+                    while(timer < 2500) {
+                        sleep(100);
+                        timer = timer +100;
+                    };
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    finish();
+                    startActivity(new Intent(MainActivity.this, SelectionActivity.class));
+                }
             }
-        }
-    };
+        };
+        thread.start();
+    }
 }
