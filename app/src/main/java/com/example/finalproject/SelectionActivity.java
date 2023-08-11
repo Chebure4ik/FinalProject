870bd17fb632900644418b7001278e1df7a7cda5
Chebure4ik <darikmlk@gmail.com>
2023-08-11 19:44:57 +0600

    commit110823.02

---
+++ app/src/main/java/com/example/finalproject/SelectionActivity.java
@@ -0,0 +1,49 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SelectionActivity extends AppCompatActivity {
+
+    private Button twoPlayerButton, threePlayerButton, fourPlayerButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selection);
+
+        twoPlayerButton = findViewById(R.id.twoPlayerButton);
+        threePlayerButton = findViewById(R.id.threePlayerButton);
+        fourPlayerButton = findViewById(R.id.fourPlayerButton);
+
+        twoPlayerButton.setOnClickListener(listener);
+        threePlayerButton.setOnClickListener(listener);
+        fourPlayerButton.setOnClickListener(listener);
+
+    }
+
+    private View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            switch (view.getId()){
+                case R.id.twoPlayerButton:
+                    Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.threePlayerButton:
+                    intent = new Intent(getApplicationContext(), ThirdActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.fourPlayerButton:
+                    intent = new Intent(getApplicationContext(), FourthActivity.class);
+                    startActivity(intent);
+                    break;
+            }
+        }
+    };
+}
